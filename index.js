c17c2e9414a1993074b63257bbb8e52a528d2931
JB Tellez <jb.tellez@gmail.com>
2018-06-28 05:59:43 -0700

    moves mongo connection out of app.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const PORT = 3000;
 const app = require('./lib/app');
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI);
+
 app.listen(PORT, () => console.log('Listening on PORT', PORT));
