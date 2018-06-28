39ec939e02b00ff0dcedaf5dbdd3a011b4510025
JB Tellez <jb.tellez@gmail.com>
2018-06-28 06:01:04 -0700

    refactors tests to use async/await

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,4 +1,4 @@
-const request = require('supertest');
+const supertest = require('supertest');
 const app = require('../lib/app');
 const mongoose = require('mongoose');
 const Mockgoose = require('mockgoose').Mockgoose;
@@ -9,15 +9,11 @@ const SIGNIN_URL = '/api/signin';
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
+const request = supertest(app);
+
 mockgoose.prepareStorage().then(function () {
     // mongoose connection	
-    mongoose.connect(process.env.MONGODB_URI);
-});
-
-it('foo should bar', () => {
-    return request(app).get('/foo').then(response => {
-        expect(response.text).toBe('bar');
-    });
+    mongoose.connect('mongodb://localhost/bearable');
 });
 
 
@@ -31,19 +27,28 @@ function getUserParams() {
 };
 
 function signUp(params) {
-    return request(app).post(SIGNUP_URL)
+    return request.post(SIGNUP_URL)
         .set('Content-Type', 'application/json')
         .send(params)
 }
 
 function signIn(params) {
-    
+
     let payload = params['username'] + ':' + params['password'];
-    
+
     let encoded = btoa(payload);
 
-    return request(app).get(SIGNIN_URL)
+    return request.get(SIGNIN_URL)
         .set('Authorization', 'Basic ' + encoded);
+}
+
+async function signUpAndIn() {
+
+    let params = getUserParams();
+
+    await signUp(params);
+
+    return signIn(params); 
 }
 
 afterEach(() => {
@@ -51,186 +56,167 @@ afterEach(() => {
 })
 
 describe('/api/signup', () => {
-    it('should return status 400 if missing username', (done) => {
+
+    it('should return status 400 if missing username', async () => {
+        
         let params = getUserParams();
+        
         delete params['username'];
 
-        signUp(params)
-            .then(response => {
-                expect(response.status).toBe(400);
-                done();
-            });
+        const response = await signUp(params);
+
+        expect(response.status).toBe(400);
+                
     });
 
-    it('should return status 400 if missing email', (done) => {
+    it('should return status 400 if missing email', async () => {
+        
         let params = getUserParams();
+        
         delete params['email'];
 
-        signUp(params)
-            .then(response => {
-                expect(response.status).toEqual(400);
-                done();
-            });
+        const response = await signUp(params);
+
+        expect(response.status).toBe(400);
     });
 
-    it('should return status 400 if missing password', (done) => {
+    it('should return status 400 if missing password', async () => {
+
         let params = getUserParams();
+        
         delete params['password'];
 
-        signUp(params)
-            .then(response => {
-                expect(response.status).toEqual(400);
-                done();
-            });
+        const response = await signUp(params);
+
+        expect(response.status).toBe(400);
     });
- 
-    it('should return 400 is username already used', (done) => {
+
+    it('should return 400 is username already used', async () => {
+        
         let params = getUserParams();
 
-        signUp(params)
-            .then(() => {
-                params.email += "something different"
-                signUp(params).then(res => {
-                    expect(res.status).toBe(400);
-                    done();
-                })
-            });
-    })
+        await signUp(params);
 
-    it('should return 400 if email already used', (done) => {
-        let params = getUserParams();
+        params.email += "something different";
 
-        signUp(params)
-            .then(() => {
-                params.username += "something different";
-                signUp(params).then(res => {
-                    expect(res.status).toBe(400);
-                    done();
-                })
-            });
-    })
-    it('should return status 200 with successful request', (done) => {
-        let params = getUserParams();
+        const response = await signUp(params);
 
-        signUp(params)
-            .then(res => {
-                expect(res.status).toEqual(200);
-                expect(res.body.password).not.toBe(params.password);
-                expect(bcrypt.compareSync(params.password, res.body.password));
-                done();
-            });
+        expect(response.status).toBe(400);
     });
+
+    it('should return 400 if email already used', async () => {
+        
+        let params = getUserParams();
+
+        await signUp(params);
+
+        params.username += "something different";
+        
+        const response = await signUp(params);
+
+        expect(response.status).toBe(400);
+
+    });
+
+    it('should return status 200 with successful request', async () => {
+
+        let params = getUserParams();
+
+        const response = await signUp(params);
+
+        expect(response.status).toEqual(200);
+        
+        expect(response.body.password).not.toBe(params.password);
+        
+        expect(bcrypt.compareSync(params.password, response.body.password));
+    });        
 });
 
 describe('/api/signin', () => {
 
-    it('should return 401 unauthorized if password is incorrect', (done) => {
-        let params = getUserParams();
-
-        signUp(params)
-            .then(res => {
-
-                // intentionally set the password as a wrong password
-                params.password = params.password + 'wrong';
-
-                return signIn(params);
-            })
-            .then(response => {
-                expect(response.status).toBe(401);
-                done();
-            });
-    });
-
-
-    it('should return 200 if username and password are ok', (done) => {
-        let params = getUserParams();
-
-        let userId;
-
-        signUp(params)
-            .then(res => {
-                expect(res.status).toBe(200);
-
-                userId = res.body._id;
-
-                return signIn(params);
-            })
-            .then(res => {
-                expect(res.status).toBe(200);
-                expect(res.body.token).toBeDefined();
-                expect(jwt.verify(res.body.token, process.env.SECRET).id).toBe(userId);
-                done();
-            });
-    });
-
-    it('should return 200 if username and password are ok at basic authorized route', (done) => {
-        let params = getUserParams();
-
-        signUp(params)
-            .then(res => {
-                return signIn(params);
-            })
-            .then(res => {
-                expect(res.status).toBe(200);
-                done();
-            });
-    });
-
-    it('should return 400 if username or password are not ok at protected route', (done) => {
-        let params = getUserParams();
-
-        signUp(params)
-            .then(() => signIn(params))
-            .then(res => {
-                expect(res.status).toBe(200);
-
-                params.password += 'mangled';
-
-                let payload = params['username'] + ':' + params['password'];
-                let encoded = btoa(payload);
-
-                return request(app).get('/basic-authorized')
-                    .set('Authorization', 'Basic ' + encoded);
-            })
-            .then(res => {
-                expect(res.status).toBe(400);
-                done();
-            });
-    });
-
-    it('should return 400 if token missing at ultra protected route', (done) => {
-        let params = getUserParams();
-
-        signUp(params)
-            .then(() => signIn(params))
-            .then(res => {
-                
-                request(app).get('/bearer-authorized')
-                    .then(result => {
-                        expect(result.status).toBe(401)
-                        done();
-                    });
-                
-            });
-    });
-
-    it('should return 200 if token good at bearer authorized route', (done) => {
+    it('should return 401 unauthorized if password is incorrect', async () => {
         
         let params = getUserParams();
 
-        signUp(params)
-            .then(() => signIn(params))
-            .then(res => {
-                
-                const token = res.body.token;
+        await signUp(params);
 
-                request(app).get('/bearer-authorized')
-                    .set('Authorization', 'Bearer ' + token)
-                    .then(result => {
-                        expect(result.status).toBe(200)
-                        done();
-                    });
+        // intentionally set the password as a wrong password
+        params.password = params.password + 'wrong';
+
+        const response = await signIn(params);
+
+        expect(response.status).toBe(401);
+    });
+
+    it('should return 200 if username and password are ok', async () => {
+
+        const response = await signUpAndIn();
+
+        expect(response.status).toBe(200);
+    });
+
+    it('should get good token if username and password are ok', async () => {
+
+        let params = getUserParams();
+
+        const signUpResponse = await signUp(params);
+
+        expect(signUpResponse.status).toBe(200);
+
+        const userId = signUpResponse.body._id;
+
+        const signInResponse = await signIn(params);
+
+        expect(signInResponse.status).toBe(200);
+
+        expect(signInResponse.body.token).toBeDefined();
+
+        expect(jwt.verify(signInResponse.body.token, process.env.SECRET).id).toBe(userId);
                 
-            });
+    });
+
+    it('should return 400 if username or password are not ok at protected route', async () => {
+        
+        let params = getUserParams();
+
+        await signUp(params);
+
+        await signIn(params);
+
+        params.password += 'mangled';
+
+        let payload = params['username'] + ':' + params['password'];
+        
+        let encoded = btoa(payload);
+
+        const response = await request
+            .get('/basic-authorized')
+            .set('Authorization', 'Basic ' + encoded);
+
+        expect(response.status).toBe(400);
+        
+    });
+
+    it('should return 400 if token missing at ultra protected route', async () => {
+        
+        await signUpAndIn();
+
+        const response = await request.get('/bearer-authorized');
+
+        expect(response.status).toBe(401);
+    });
+
+    it('should return 200 if token good at bearer authorized route', async () => {
+
+        const signInResponse = await signUpAndIn();
+
+        const token = signInResponse.body.token;
+
+        const response = await request
+            .get('/bearer-authorized')
+            .set('Authorization', 'Bearer ' + token);
+
+        expect(response.status).toBe(200);
+
     });
 });
